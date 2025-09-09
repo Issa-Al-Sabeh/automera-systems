930ceb0ac33d2eede0a8f67dd9abfedb59675f99
IssaAlSabeh <issasab2204@gmail.com>
2025-09-09 14:30:25 +0300

    Added Next.js config for environment variables and improved Amplify build logging

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    SES_REGION: process.env.SES_REGION,
+    SES_FROM_EMAIL: process.env.SES_FROM_EMAIL,
+    SES_TO_EMAIL: process.env.SES_TO_EMAIL,
+    SES_ACCESS_KEY_ID: process.env.SES_ACCESS_KEY_ID,
+    SES_SECRET_ACCESS_KEY: process.env.SES_SECRET_ACCESS_KEY,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@aws-sdk/client-ses'],
+  },
+}
+
+module.exports = nextConfig
